ff78ee881657d2b481ca41e162d5a43a5c36be88
Angelica Lazaro <angelicamld@gmail.com>
2024-10-22 15:25:10 +0200

    ajout de Vulpix

---
+++ src/composants/PokemonCard.tsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img
+        src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/037.png"
+        alt="Vulpix poke"
+      />
+      <figcaption> "Vulpix"</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
